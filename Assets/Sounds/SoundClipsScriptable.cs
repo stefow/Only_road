8f409c365d1899677abff48fdf230a5743998d9d
Stefan Uglješić <stefan.ugljesic98@hotmail.com>
2025-03-25 18:25:46 +0100

    Stable 1

--- Assets/Sounds/SoundClipsScriptable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Sounds", menuName = "SoundsSriptable", order = 1)]
-public class SoundClipsScriptable : ScriptableObject
-{
-    public List<AudioClip> audioClips;
-}
